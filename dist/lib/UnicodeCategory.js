036164a576f9a8b612edcbbb87b42c486be4dde6
kyranet <contact@skyra.pw>
2020-07-24 08:15:08 +0000

    build: tsc build for 7990aa77d110a29257c87f8c45ad4e9c3f266cc1

--- dist/lib/UnicodeCategory.js
+++
@@ -1,4 +0,0 @@
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-// https://github.com/dotnet/runtime/blob/master/src/libraries/System.Private.CoreLib/src/System/Globalization/UnicodeCategory.cs
-//# sourceMappingURL=UnicodeCategory.js.map
